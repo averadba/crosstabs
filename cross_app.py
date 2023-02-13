302649ab96456dd7fc14161cff8dc36a20bf5ccd
Alexis Vera <76126512+averadba@users.noreply.github.com>
2023-02-13 07:29:32 -0400

    Initial commit
    Source code and requirements file

---
+++ cross_app.py
@@ -0,0 +1,49 @@
+import streamlit as st
+import pandas as pd
+from scipy.stats import chi2_contingency, fisher_exact
+import numpy as np
+
+st.markdown(""" <style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+</style> """, unsafe_allow_html=True)
+
+st.set_option('deprecation.showPyplotGlobalUse', False)
+
+# Create a Streamlit interface for file upload and variable selection
+st.title("Categorical Variable Analysis")
+st.write("*By*: A. Vera")
+
+st.write("This app allows you to perform a preliminary analysis of categorical variables in a CSV file." 
+          "First, upload a CSV file that contains categorical variables. The app will automatically detect the categorical variables and allow you to select"
+          " which ones you want to analyze. The app will then perform pairwise cross-tabulations of the selected variables and display the contingency tables and"
+          " corresponding test results. This analysis is useful for exploring the relationships between pairs of variables, but additional analysis may be required"
+          " to fully understand the relationships between the variables.")
+
+file = st.file_uploader("Upload a CSV file")
+if file is not None:
+    data = pd.read_csv(file)
+    categorical_cols = list(data.select_dtypes(include=["object"]).columns)
+    selected_cols = st.multiselect("Select categorical variables to analyze", categorical_cols)
+
+    # Convert selected columns to category data type if necessary
+    for col in selected_cols:
+        if not pd.api.types.is_categorical_dtype(data[col]):
+            data[col] = data[col].astype("category")
+
+    # Perform pairwise cross-tabulations and display results
+    st.header("Cross-tabulations")
+    for i, col1 in enumerate(selected_cols):
+        for col2 in selected_cols[i+1:]:
+            crosstab = pd.crosstab(data[col1], data[col2])
+            st.write(f"Cross-tabulation of {col1} and {col2}")
+            st.write(crosstab)
+            chi2, pval, dof, exp_freq = chi2_contingency(crosstab)
+            if crosstab.shape == (2, 2):
+                oddsratio, pval = fisher_exact(crosstab)
+                st.write(f"Fisher's exact test statistic: {oddsratio}, p-value: {pval}")
+            elif np.any(exp_freq < 5):
+                st.write("Warning: chi-square test may be invalid due to expected frequency less than 5")
+                st.write(f"Chi-square test statistic: {chi2}, p-value: {pval}")
+            else:
+                st.write(f"Chi-square test statistic: {chi2}, p-value: {pval}")
