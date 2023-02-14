cac9b652ca0abb2e1dd3a8fc02a7b6eb27057d40
Alexis Vera <76126512+averadba@users.noreply.github.com>
2023-02-14 09:46:57 -0400

    Update cross_app.py

--- cross_app.py
+++ cross_app.py
@@ -50,5 +50,5 @@ if file is not None:
                 st.write(f"Chi-square test statistic: {round(chi2, 4)}, p-value: {round(pval, 4)}")
                 
 st.sidebar.header("Cross-tabulations")
-    for tab in tabs_list:
-        st.sidebar.write(f"[{tab[0]} x {tab[1]}](#{tab[0]}{tab[1]})")
+for tab in tabs_list:
+    st.sidebar.write(f"[{tab[0]} x {tab[1]}](#{tab[0]}{tab[1]})")
